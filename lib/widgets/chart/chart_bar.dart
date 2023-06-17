ac8e10b17e689eab0e378adde572bed85fe7dbec
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2023-06-17 16:13:30 +0530

    minor correction

--- lib/widgets/chart/chart_bar.dart
+++ lib/widgets/chart/chart_bar.dart
@@ -10,7 +10,7 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isDarkMode =
+    final bool isDarkMode =
         MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Expanded(
       child: Padding(
